e299fdbd1a0f7427a07a4ac9067c89610c2db62e
Viet.68 <tanviet060801@gmail.com>
2021-11-17 04:01:22 +0700

    First version of application moved into GitHub

---
+++ public/js/product_detail/main.js
@@ -0,0 +1,89 @@
+(function ($) {
+    "use Strict";
+    /*----------------------------------------------------
+    Hot Deal Product Activation
+    -----------------------------------------------------*/
+    $('.hot-deal-active').owlCarousel({
+        loop: false,
+        nav: true,
+        dots: false,
+        smartSpeed: 1500,
+        navText: ["<i class='lnr lnr-arrow-left'></i>", "<i class='lnr lnr-arrow-right'></i>"],
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1,
+                autoplay: true,
+                smartSpeed: 500
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 5
+            }
+        }
+    })
+    $('.hot-deal-active3').owlCarousel({
+        loop: false,
+        nav: true,
+        dots: false,
+        smartSpeed: 1500,
+        navText: ["<i class='lnr lnr-arrow-left'></i>", "<i class='lnr lnr-arrow-right'></i>"],
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1,
+                autoplay: true,
+                smartSpeed: 500
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    })
+
+    /*-------------------------------------
+    Thumbnail Product activation
+    --------------------------------------*/
+    $('.thumb-menu').owlCarousel({
+        loop: false,
+        navText: ["<i class='lnr lnr-arrow-left'></i>", "<i class='lnr lnr-arrow-right'></i>"],
+        margin: 15,
+        smartSpeed: 1000,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 3,
+                autoplay: true,
+                smartSpeed: 500
+            },
+            768: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+    $('.thumb-menu a').on('click', function () {
+        $('.thumb-menu a').removeClass('active');
+    })
+
+})(jQuery);
